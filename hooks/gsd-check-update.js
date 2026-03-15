655bf3bdaa19e308cf8071e0a3045b344955a7a2
Howlil <2211521003_ulil@student.unand.ac.id>
2026-03-15 14:27:10 +0700

    chore: customize package references to gsdm fork

--- hooks/gsd-check-update.js
+++ hooks/gsd-check-update.js
@@ -61,7 +61,7 @@ const child = spawn(process.execPath, ['-e', `
 
   let latest = null;
   try {
-    latest = execSync('npm view get-shit-done-cc version', { encoding: 'utf8', timeout: 10000, windowsHide: true }).trim();
+    latest = execSync('npm view gsdm version', { encoding: 'utf8', timeout: 10000, windowsHide: true }).trim();
   } catch (e) {}
 
   const result = {
